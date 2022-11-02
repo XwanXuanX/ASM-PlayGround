da9d0d9651418ea31b949773e9dbed5a5c982a79
Wanxuan Li <lzlu1018@gmail.com>
2022-11-02 02:48:54 -0400

    selection sort refined

--- sorting/select_sort/select_sort.asm
+++ sorting/select_sort/select_sort.asm
@@ -13,53 +13,92 @@ output 	resb 30
 out_len	equ 30
 
 	section .text
-_start:
-	mov rdx, Arr
-	; select sort begin
-	xor r8, r8	; int i = 0
+Select_Sort:
+	;function prologue
+	sub rsp, 8	; make room for a 8bytes local var
+	push rbx
+	push rbp
+	push r12
+	push r13
+	; function body
+	lea rbx, [rsi - 1]	; Arr_len - 1
+	mov [rsp + 32], rbx
+	xor rbx, rbx	; int i = 0
 s_LP1:
-	cmp r8, (Arr_len - 1)
-	jnl cont
-	mov r10, r8	; r10 = smallest index = i
-	lea r9, [r8 + 1]	; int j = i + 1
+	cmp rbx, [rsp + 32]
+	jnl s_epilogue
+	mov r12, rbx	; assume r12 is the smallest
+	lea rbp, [rbx + 1]	; int j = i + 1
 s_LP2:
-	cmp r9, Arr_len
+	cmp rbp, rsi
 	jnl s_LP2_end
-	mov al, byte[rdx + r9]	; al = Arr[j]
-	cmp al, byte[rdx + r10]
+	mov r13b, byte[rdi + rbp]
+	cmp r13b, byte[rdi + r12]
 	jnl s_cont1
-	mov r10, r9
+	mov r12, rbp	; update smallest index
 s_cont1:
-	inc r9
+	inc rbp
 	jmp s_LP2
 s_LP2_end:
-	cmp r10, r8
+	cmp r12, rbx
 	je s_cont2
-	mov al, byte[rdx + r8]
-	push rax
-	mov al, byte[rdx + r10]
-	mov byte[rdx + r8], al
-	pop rax
-	mov byte[rdx + r10], al
+	mov r13b, byte[rdi + rbx]
+	push r13
+	mov r13b, byte[rdi + r12]
+	mov byte[rdi + rbx], r13b
+	pop r13
+	mov byte[rdi + r12], r13b
 s_cont2:
-	inc r8
+	inc rbx
 	jmp s_LP1
-cont:
-	xor r8, r8
-	mov rdi, output
-o_LP:
-	cmp r8, Arr_len
-	jnl print
-	mov r9b, byte[rdx + r8]
-	add r9b, 0x30
-	mov byte[rdi], r9b
-	inc rdi
-	mov byte[rdi], 32
-	inc rdi
-	inc r8
-	jmp o_LP
-print:
-	mov byte[rdi], 10
+s_epilogue:
+	pop r13
+	pop r12
+	pop rbp
+	pop rbx
+	add rsp, 8
+	ret
+
+Print:
+	; function prologue
+	push rbx	; counter
+	push r12	; tmp var
+	push rbp	; output pointer
+	; function body
+	mov rbp, rdx
+	xor rbx, rbx	; int i = 0
+p_LP:
+	cmp rbx, rsi
+	je p_epilogue
+	mov r12b, byte[rdi + rbx]
+	add r12, 0x30	; add zero
+	mov byte[rbp], r12b
+	inc rbp
+	mov byte[rbp], 32
+	inc rbp
+	inc rbx
+	jmp p_LP
+p_epilogue:
+	mov byte[rbp], 10
+	; function epilogue
+	pop rbp
+	pop r12
+	pop rbx
+	ret
+
+_start:
+	; Select_Sort function takes two parameters
+	mov rdi, Arr		; the pointer to the first element in the array
+	mov rsi, Arr_len	; the length of the array
+	call Select_Sort
+
+	; Print function takes three parameters
+	mov rdi, Arr		; the pointer to the arr first element
+	mov rsi, Arr_len	; the length of the array
+	mov rdx, output		; the pointer to the output first element
+	call Print
+
+	; invoke syscall
 	mov rax, 1
 	mov rdi, rax
 	mov rsi, output
